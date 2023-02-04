2513ec44fa6b77e2472dee6edfbfd3285ed633cc
noel <noelantoine974@outlook.fr>
2023-02-04 21:18:19 +0400

    mise

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//Import style 
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Import des compartiment pages et components 
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+       <Home /> 
+    </div>
+  );
+}
+
+export default App;
