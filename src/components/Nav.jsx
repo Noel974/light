b994f4b3fbf19c98df0def7b0589582c202f39ba
noel <noelantoine974@outlook.fr>
2023-02-08 09:51:50 +0400

    form

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../assets/img/logo.svg';
 
 <Navbar bg="light" variant="light">
     <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={Logo} alt="Logo Light Map" /></Navbar.Brand>
         <Nav className="me-auto">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">Features</Nav.Link>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Form from 'react-bootstrap-Form';
+
+function Home() {
+    return (
+        <><>
+            //Carousel
+            <section>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=First slide&bg=373940"
+                            alt="First slide" />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Illuminons vos parcours.Avec l'application LightMap, Concevez simplement vos AFEST et concentrez vous sur l’essentiel.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Second slide&bg=282c34"
+                            alt="Second slide" />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            alt="Third slide" />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </section>
+        </><>
+             // Formulaire
+                <section>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                            <Form.Label>Names</Form.Label>
+                            <Form.Control type="name" placeholder="name" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                    </Form>
+                </section>
+            </></>
+    
+    );
+}
+
+export default Home;
